9c8e7457fc5969074546d1ce7df90facd3497a83
kabirmia526@gmail.com <kabirmia526@gmail.com>
2025-03-28 22:36:19 +0600

    first change

---
+++ src/test/java/comCICDTest/CicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package comCICDTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
